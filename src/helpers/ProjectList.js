e8aac7dcfeac606bd6de9f9a147c0b721a9478c5
Andrew Kimwetich <kimwetichandrew@gmail.com>
2023-09-12 18:53:23 +0300

    desc

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -6,6 +6,6 @@ export const ProjectList = [
     name: "Artists Profile Builder App",
     image: proj1,
     skills: "React, NodeJs, Express,ContextApi,MongoDb,Mern-stack",
-    desc: "An application with authentication. Users can create profiles for their favorite artists and upload it online. Users can save their favorite profiles and view them in favorites tab.",
+    desc: "An app with authentication. Allows user to build a profile for their favorite artists (name, image-Url ,genre, country and a description). Also users can save Artists-profile to the favorites and view them in favorites tab.",
   },
 ];
